3627813520df2f9cfaf540c56f808b226dad8d60
Jason Merrell <112509082+merrellj-codeup@users.noreply.github.com>
2022-09-29 13:58:04 -0500

    testing react solution

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 import react from 'https://cdn.skypack.dev/react';
+
+const {
+    BrowserRouter,
+    Route,
+    Link
+  } = ReactRouterDOM
+  
+  const App = () => (
+    <div>
+      <h1>React Router Example</h1>
+      <ul role="nav">
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+       
+      <div>
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/profile' component={Profile} />
+      </div>
+    </div>
+  )
+   
+  const Dashboard= () => <h2>You are in the Dashboard</h2>
+  const Profile= () => <h2>You are in the Profile</h2>
+  
+  
+  ReactDOM.render((
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  ), document.getElementById('root'));
