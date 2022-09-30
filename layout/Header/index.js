0d3d6e0fd452f9417659e6555c36e0f8b28c4df0
Jason Merrell <jason.merrell@codeup.com>
2022-09-30 11:14:37 -0500

    transitioning to react

--- layout/Header/index.js
+++ layout/Header/index.js
@@ -1,7 +1,23 @@
 import { html } from 'https://cdn.skypack.dev/htm/react';
 import react from 'https://cdn.skypack.dev/react';
+import styledComponents from 'https://cdn.skypack.dev/styled-components';
+
 const logo = '/images/CodeupFullColorLogo.png';
 
+const HeaderWrapper = styledComponents.div`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  height: 100px;
+  padding-right: 30px;
+  padding-left: 30px;
+  justify-content: space-between;
+  align-items: center;
+  flex: 0 0 auto;
+  border-bottom: 1px solid #e0e0e0;
+  background-color: #fff;
+`
+console.log(HeaderWrapper);
 export function Header() {
   return (
     html`
